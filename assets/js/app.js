1ea31614245c80dbd2a4fce782937db4ae8101f0
Filip Bengtegård <bengtegardbook@gmail.com>
2016-01-17 12:53:11 +0100

    great start

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  
+    $(".button-collapse").sideNav();
+  
+    $('select').material_select();
+  
+    $(".dropdown-button").dropdown();
+
+    $('.modal-trigger').leanModal();
+    
+    $('.scrollspy').scrollSpy();
+
+});
